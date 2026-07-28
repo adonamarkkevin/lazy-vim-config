49af58e1ab9b4eb36fc6e11a7c29d101bba2e01a
Mark Kevin Adona <adonamarkkevin@gmail.com>
2026-07-29 00:19:50 +0800

    feat(editor): set 4-width tabs for go filetype

--- lua/editor.lua
+++ lua/editor.lua
@@ -33,3 +33,14 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.treesitter.stop()
     end,
 })
+
+-- Go: real tabs, 4-wide (gofmt convention)
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    callback = function()
+        vim.opt_local.expandtab = false
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.shiftwidth = 4
+    end,
+})

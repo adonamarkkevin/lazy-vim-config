a1456f837eac545fbe7c64f1b1d71a11a0432788
Mark Kevin Adona <adonamarkkevin@gmail.com>
2026-08-03 13:27:09 +0800

    refactor(vim-rest-console): execute keymap now is rr

--- lua/plugins/vim-rest-console.lua
+++ lua/plugins/vim-rest-console.lua
@@ -5,11 +5,12 @@ return {
         vim.g.vrc_set_default_mapping = 0
         vim.g.vrc_response_default_content_type = "application/json"
         vim.g.vrc_output_buffer_name = "_OUTPUT.json"
+        vim.g.vrc_curl_opts = { ["-s"] = "" }
         vim.g.vrc_auto_format_response_patterns = {
             json = "jq",
         }
     end,
     config = function()
-        vim.keymap.set("n", "<leader>xr", "<Plug>RestConsoleFullRequest", { remap = true })
+        vim.keymap.set("n", "<leader>rr", ":call VrcQuery()<CR>", { buffer = false, silent = true })
     end,
 }

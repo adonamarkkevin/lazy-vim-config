bb29baebc0f6fc6827f74cab145f0884c02e1909
Mark Kevin Adona <adonamarkkevin@gmail.com>
2026-09-03 17:34:28 +0800

    fix(theme): restore neo-tree git status colors on onedark

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -8,6 +8,19 @@ return {
             },
         })
         vim.opt.laststatus = 3 -- global statusline (was kanagawa's globalStatus)
+
+        -- onedark leaves neo-tree's git-status groups near Comment-grey, so
+        -- modified/untracked files are hard to spot in the tree. Re-color them.
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "onedark*",
+            callback = function()
+                vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#e5c07b" })
+                vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#56b6c2", italic = true })
+                vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#98c379" })
+                vim.api.nvim_set_hl(0, "NeoTreeGitConflict", { fg = "#e06c75" })
+            end,
+        })
+
         vim.cmd.colorscheme("onedark")
     end,
 }
